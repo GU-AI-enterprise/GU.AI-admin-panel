a586d43cdecf65e9453be4a368b47795d7af4213
Hoàng Tấn Đạt <154804103+dathoang1412@users.noreply.github.com>
2026-05-27 10:34:37 +0700

    refactor: redux x auth implementation

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import appReducer from './slices/appSlice';
+
 
 export const store = configureStore({
   reducer: {
-    // Add your reducers here
+    app: appReducer,
   },
   devTools: process.env.NODE_ENV !== 'production',
 });
